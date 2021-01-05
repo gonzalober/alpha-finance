2046b1bb4b2eaaa9519a3da22ec4348cc170d880
gonzalober <gonzaloberhouet@gmail.com>
2021-01-05 16:18:59 +0000

    Displayinfo and ticker filter

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -1,18 +1,57 @@
 import React from "react";
 import { useFetch } from "./useFetch.js";
+import { useState, useEffect } from "react";
 
 export default function StockRow() {
   const { data, loading } = useFetch(
-    "https://cloud.iexapis.com/stable/stock/FB/chart/date/20201111?token=pk_32c3cc0e5efd4e45847b33e0369afb60"
+    "https://cloud.iexapis.com/stable/stock/market/batch?symbols=goog,fb&types=chart&range=1m&last=5&token=pk_32c3cc0e5efd4e45847b33e0369afb60"
   );
+  const [table, setTable] = useState([]);
+  //  "https://cloud.iexapis.com/stable/stock/FB/chart/date/20201111?token=pk_32c3cc0e5efd4e45847b33e0369afb60"
+  console.log(data);
 
+  function ticker(input) {
+    console.log(input);
+    let stockFiltered = data.filter((tickersymbol) => tickersymbol === input);
+    setTable(ditanceFiltered.sort((a, b) => (a.time < b.time ? -1 : 1)));
+  }
   const border = {
     borderColor: "grey",
     marginBottom: "20px",
   };
+  const DisplaInfo = () => {
+    if (data) {
+      for (let key in data) {
+        let value = data[key];
+        // for (const [stock, value] of Object.entries(data)) {
+        //   console.log(stock, value);
+        return (
+          <tr style={border}>
+            <td>{key}</td>
+            <td>{console.log(key)}</td>
+            <td>{console.log(value)}</td>
+            <td>{console.log(value.chart)}</td>
+            <td>{value.chart.close}</td>
+            <td>{value.date}</td>
+            <td>{value.label}</td>
+          </tr>
+        );
+      }
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
+      <p>Enter the ticker: </p>
+      <input type="text" defaultValue="" />
+      <input
+        onClick={() => ticker()}
+        className="form-submit"
+        type="submit"
+        value="ticker"
+      />
       <table className="table nt-5">
         <thead>
           <tr>
@@ -22,19 +61,7 @@ export default function StockRow() {
             <th>Time</th>
           </tr>
         </thead>
-        <tbody>
-          {console.log(data)}
-          {loading
-            ? "loading..."
-            : data.map((stock) => (
-                <tr style={border} key={stock.id}>
-                  <td>{"FB"}</td>
-                  <td>{stock.close}</td>
-                  <td>{stock.date}</td>
-                  <td>{stock.label}</td>
-                </tr>
-              ))}
-        </tbody>
+        <tbody>{DisplaInfo()}</tbody>
       </table>
     </div>
   );
--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { iex } from "../config/iex.js";
 
-//const url = `${iex.base_url}/stock/${this.props.ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`;
+//+const url = `${iex.base_url}/stock/${this.props.ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`;
 export const useFetch = (url) => {
   const [data, setData] = useState({ data: null, loading: true });
-  useEffect(() => {
-    setData({ data: null, loading: true });
-    fetch(url)
+  useEffect(async () => {
+    setData((state) => ({ data: state.data, loading: true }));
+    await fetch(url)
       .then((x) => x.json())
       .then((y) => {
         setData({ data: y, loading: false });
